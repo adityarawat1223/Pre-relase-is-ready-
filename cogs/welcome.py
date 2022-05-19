261fb5971b033079236c8b2bc304b1355e7d030b
adityarawat1223 <80033471+adityarawat1223@users.noreply.github.com>
2022-05-19 12:42:43 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -18,11 +18,11 @@ class welcome(commands.Cog):
     async def on_member_join(self, member):
         channel = channel = self.bot.get_channel(976739222700568596)
 
-        embed = discord.Embed(color=0x55a7f7, timestamp=datetime.datetime.utcnow(), title=f"Hey ! {member.mention} Welcome To My Support Server",
-                                description=f"════════════════\n\n:white_check_mark: Before You Start Introduce Yourself in <#974312751029706782>\n\n :unlock: check rules in <#974312732042096650>\n\n :recycle: Announcement in <#974312736932659270>\n\n════════════════\n\n Thank You For Joining Server"
+        embed = discord.Embed(color=0x55a7f7, timestamp=datetime.datetime.utcnow(), title=" Welcome To My Support Server",
+                                description=f"{member.mention}\n\n════════════════\n\n:white_check_mark: Before You Start Introduce Yourself in <#974312751029706782>\n\n :unlock: check rules in <#974312732042096650>\n\n :recycle: Announcement in <#974312736932659270>\n\n════════════════\n\n Thank You For Joining Server"
                                 )
         embed.set_image(
-                url="https://imgur.com/2D9qLj1")
+                url="https://i.imgur.com/2D9qLj1.gif")
         await channel.send(embed=embed)
 
 #end
