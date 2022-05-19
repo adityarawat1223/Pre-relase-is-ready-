546ccc3b99b0c649d7a81b36d140561447708392
adityarawat1223 <80033471+adityarawat1223@users.noreply.github.com>
2022-05-19 12:59:07 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -24,6 +24,13 @@ class welcome(commands.Cog):
         embed.set_image(
                 url="https://i.imgur.com/2D9qLj1.gif")
         await channel.send(embed=embed)
+        
+    @app_commands.command(name="echo", description="Bot will send a message for you")
+    @app_commands.checks.has_permissions(ban_members=True)
+    async def echo(self, interaction: discord.Interaction,  content: str , channel: discord.TextChannel) -> None:
+        await interaction.response.defer(ephemeral=True, thinking=True)
+        await channel.send(content)
+        await interaction.followup.send("<a:dvloper:974560024200351744> Done")
 
 #end
 async def setup(bot) -> None:
